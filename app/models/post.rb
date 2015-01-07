7240a1fac713894280db68f8ff63f35e8474fac4
Rounaq Jhunjhunu Wala <rounaqwl66@gmail.com>
2015-01-07 12:41:49 +0530

    Maintenance Work

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-<<<<<<< HEAD
   has_many :blocks, dependent: :destroy
-=======
-  has_many :blocks
->>>>>>> post-blocks
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
