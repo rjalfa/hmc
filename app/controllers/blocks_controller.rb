9d1863893f8a5cf9188b85ef89c17e4d5d60ae48
Rounaq Jhunjhunu Wala <rounaqwl66@gmail.com>
2015-01-11 17:22:49 +0530

    Git Fix

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,21 +1,8 @@
 class BlocksController < ApplicationController
-<<<<<<< HEAD
 
-  def create
-  	
-  end
-
-  private
-
-  	def block_params
-      params.require(:block).permit(:content)
-    end   
-end
-=======
   def destroy
   	if @block.comments
   		@block.comments.destroy
   	@block.destroy
   end
 end
->>>>>>> post-blocks
